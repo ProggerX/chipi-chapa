8d8c97de9a5f824bfbd9e48e4b4b66141f3ef34c
ProggerX <x@proggers.ru>
2025-10-08 10:40:13 +0300

    Fix static build in CI

--- flake.nix
+++ flake.nix
@@ -4,14 +4,27 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
   outputs =
-    { flake-parts, nixpkgs, ... }@inputs:
+    {
+      flake-parts,
+      nixpkgs,
+      self,
+      ...
+    }@inputs:
     let
+      fixGHC =
+        pkg:
+        pkg.override {
+          enableRelocatedStaticLibs = true;
+          enableShared = false;
+          enableDwarf = false;
+        };
       hs-project =
         {
           pkgs,
+		  hp ? pkgs.haskellPackages,
           isShell ? false,
         }:
-        pkgs.haskellPackages.developPackage {
+        hp.developPackage {
           root = ./.;
           returnShellEnv = isShell;
           modifier =
@@ -30,15 +43,20 @@
       perSystem =
         { pkgs, ... }:
         {
-		  packages.default = hs-project { inherit pkgs; };
-          packages.static = pkgs.haskell.lib.overrideCabal (hs-project { inherit pkgs; }) (old: {
+          packages.default = hs-project { inherit pkgs; };
+          packages.static = pkgs.haskell.lib.overrideCabal (hs-project { inherit pkgs; hp = (pkgs.haskellPackages.override { ghc = fixGHC pkgs.ghc; }); }) (old: {
             enableSharedExecutables = false;
             enableSharedLibraries = false;
             configureFlags = [
               "--ghc-option=-optl=-static"
               "--extra-lib-dirs=${pkgs.gmp6.override { withStatic = true; }}/lib"
               "--extra-lib-dirs=${pkgs.zlib.static}/lib"
-              "--extra-lib-dirs=${pkgs.libffi.overrideAttrs (old: { dontDisableStatic = true; doChecks = false; })}/lib"
+              "--extra-lib-dirs=${
+                pkgs.libffi.overrideAttrs (old: {
+                  dontDisableStatic = true;
+                  doChecks = false;
+                })
+              }/lib"
             ];
           });
           devShells.default = hs-project {
