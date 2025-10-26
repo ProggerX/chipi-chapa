a807e9e061212c8c939328dd05ab61dfe7497ccc
ProggerX <x@proggers.ru>
2025-10-26 12:37:04 +0300

    fix: Update flake

--- flake.nix
+++ flake.nix
@@ -17,29 +17,28 @@
           hp ? pkgs.haskellPackages,
           isShell ? false,
         }:
-        (hp.developPackage {
-          root = ./.;
-          returnShellEnv = isShell;
-          modifier =
-            drv:
-            pkgs.haskell.lib.addBuildTools drv (
-              with pkgs;
-              [
-                cabal-install
-                haskell-language-server
-              ]
-            );
-        }).overrideAttrs {
-			nativeBuildInputs = [ pkgs.makeWrapper pkgs.removeReferencesTo ];
-			postInstall = ''wrapProgram $out/bin/chipi-chapa --set LD_LIBRARY_PATH ${pkgs.lib.makeLibraryPath [ pkgs.alsa-lib ]}'';
-		};
-    in
+	hp.developPackage {
+		root = ./.;
+		returnShellEnv = isShell;
+		modifier =
+			drv:
+			pkgs.haskell.lib.addBuildTools drv (
+					with pkgs;
+					[
+					cabal-install
+					haskell-language-server
+					]
+					);
+	}; in
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = nixpkgs.lib.platforms.unix;
       perSystem =
         { pkgs, ... }:
         {
-          packages.default = hs-project { inherit pkgs; };
+          packages.default = (hs-project { inherit pkgs; }).overrideAttrs {
+			nativeBuildInputs = [ pkgs.makeWrapper pkgs.removeReferencesTo ];
+			postInstall = ''wrapProgram $out/bin/chipi-chapa --set LD_LIBRARY_PATH ${pkgs.lib.makeLibraryPath [ pkgs.alsa-lib ]}'';
+		  };
           devShells.default = hs-project {
             inherit pkgs;
             isShell = true;
